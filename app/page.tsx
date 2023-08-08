476d64328970449dfb0e56b133bb11751b1e808f
Sokolinyy <kostyk.vasilii@gmail.com>
2023-08-08 19:12:23 +0200

    Display data from Redux Toolkit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
-import RootLayout from "./layout";
+"use client";
 
-export default function Home() {
+// pages/otherPage.js
+import React from "react";
+import { useSelector } from "react-redux";
+import { setWeatherData, setInputValue } from "@/redux/slices/weatherSlice";
+
+const WeatherDisplay = () => {
+  const weatherData = useSelector((state: any) => state.weather.weatherData);
+  const inputValue = useSelector((state: any) => state.weather.inputValue);
+
+  const kelvinToCelsius = (kelvin: number) => {
+    return kelvin - 273.15;
+  };
+
   return (
-    <main>
-      <h1>hello</h1>
-    </main>
+    <>
+      {weatherData && (
+        <div>
+          <h1>Weather for {weatherData.name}</h1>
+          {/* Display weather data here */}
+          <p>
+            Temperature:{" "}
+            {weatherData
+              ? kelvinToCelsius(weatherData.main.temp).toFixed(2)
+              : "Loading..."}
+            °C
+          </p>
+          {/* Add more weather details as needed */}
+        </div>
+      )}
+    </>
   );
-}
+};
+
+export default WeatherDisplay;
